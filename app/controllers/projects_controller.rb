f1ca5b7974b41c3bf0101be267151dc604920414
Hilmar Orth <hm.orth@aol.com>
2018-11-15 18:21:46 +0800

    didnt work

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,16 @@
+
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
+    #--------- Octokit Test area -----------
+    @client = Octokit::Client.new
+
+
+
+
+
+    #---------- Test Area end -------------
   end
 
   def destroy
