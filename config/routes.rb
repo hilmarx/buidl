2da3a595376df294d8ff4ceb28d1a7fb8cb9adea
Owain Skyrme <owaingskyrme@gmail.com>
2018-11-15 16:35:55 +0800

    deleted the generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
-  get 'project_follows/new'
-  get 'project_follows/create'
-  get 'project_follows/destroy'
-  get 'project_technologies/new'
-  get 'project_technologies/create'
-  get 'project_technologies/destroy'
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/destroy'
-  get 'user_follows/new'
-  get 'user_follows/create'
-  get 'user_follows/destroy'
-  get 'user_technologies/new'
-  get 'user_technologies/create'
-  get 'user_technologies/destroy'
-  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
