82ccb7be7e379ee94e9c265cc07a72b0cfdfa77d
Hilmar Orth <hm.orth@aol.com>
2018-11-16 15:05:45 +0800

    bla

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,11 +8,11 @@ class PagesController < ApplicationController
   end
 
   def test
-    #if User.find_by(username: "hilmarx").nil?
+    if User.find_by(username: "hilmarx").nil?
       @user = User.create(username: "hilmarx", email: "h@h.h", password: "hhhhhh")
       @user.fetch_github
-    #else
-      #@user = User.find_by(username: "hilmarx")
-    #end
+    else
+      @user = User.find_by(username: "hilmarx")
+    end
   end
 end
