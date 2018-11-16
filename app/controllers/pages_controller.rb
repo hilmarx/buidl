ba85abbc4c83a4da6e4788cabea0d5ade58bfa09
Hilmar X <hm.orth@aol.com>
2018-11-16 17:25:07 +0800

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :search, :test]
 
   def home
-    @user = User.new
     @profile = Profile.new
     # username = params[:first_name]
   end
@@ -10,8 +9,4 @@ class PagesController < ApplicationController
   def search
   end
 
-  def test
-      @user = User.create(username: "ctrlxie")
-      @user.fetch_github
-  end
 end
