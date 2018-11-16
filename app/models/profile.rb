a5a91dd914d9314e0d7221b51ba0ea887eea1466
Hilmar X <hm.orth@aol.com>
2018-11-16 17:31:52 +0800

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,24 +4,26 @@ require 'open-uri'
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
 
-  has_many :leaders, class_name: "UserFollow", foreign_key: "follower_id"
-  has_many :followers, class_name: "UserFollow", foreign_key: "leader_id"
+  has_many :leaders, class_name: "UserFollow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, class_name: "UserFollow", foreign_key: "leader_id", dependent: :destroy
 
 
-  has_many :user_technologies
+  has_many :user_technologies, dependent: :destroy
   has_many :technologies, through: :user_technologies
-  has_many :project_follows
+  has_many :project_follows, dependent: :destroy
   has_many :projects, through: :project_follows
 
-  has_many :contributions
+  has_many :contributions, dependent: :destroy
   has_many :projects, through: :contributions
 
   before_save :fetch_github
 
-
+  private
+  
   def fetch_github
-    github_username = self.github_username
-    url = "https://api.github.com/users/ctrlxie?access_token=62d50e2cbc06a841f952d8fc70b7bd2b2c7917de"
+  
+    github_username = self.username
+    url = "https://api.github.com/users/#{github_username}"
     github_profile_serialized = open(url).read
     github_profile = JSON.parse(github_profile_serialized)
 
@@ -30,10 +32,12 @@ class Profile < ApplicationRecord
     self.description = github_profile['bio']
     self.full_name = github_profile['name']
 
-    github_repos_serialized = open("https://api.github.com/users/#{github_username}/repos?access_token=62d50e2cbc06a841f952d8fc70b7bd2b2c7917de").read
+    github_repos_serialized = open("https://api.github.com/users/#{github_username}/repos").read
     github_repos = JSON.parse(github_repos_serialized)
 
     github_repos.each do |repo|
+
+      # Create Project Instance
       project = Project.new(
         name: repo['name'],
         private: repo['private'],
@@ -44,21 +48,40 @@ class Profile < ApplicationRecord
         url: repo['homepage'],
         owner_id: repo['owner']['id'],
         github_id: repo['id'],
-        url: repo['html_url']
+        address: repo['location']
         )
       project.save!
+      self.projects << project
 
-
-    # t.string "url"
-    # t.string "photo"
-    # t.integer "owner_id"
-
-    # t.integer "lines_added"
-    # t.integer "lines_deleted"
-    # t.integer "commits"
-    # t.boolean "archived"
-    # t.string "full_name"
+      # Find all technologies of a project
+      techs_serialized = open("https://api.github.com/repos/#{github_username}/#{project.name}/languages").read
+      techs = JSON.parse(techs_serialized)
+      techs.each do |tech|
+        lang = tech.first.downcase
+        size_bytes = tech.last
+        unless Technology.find_by(name: lang).nil?
+          proj_tech = ProjectTechnology.new(project_id: project.id, technology_id: Technology.find_by(name: lang).id, size_bytes: size_bytes).save!
+        end
+      end
 
     end
   end
+
+  def top_languages
+    # Calculations for finding the most used programming langugages in all repos owned by a profile
+    hash = {}
+    self.projects.each do |project|
+      project.project_technologies.each do |project_tech|
+        size = project_tech.size_bytes
+        name = Technology.find(project_tech.technology_id).name
+        if hash.has_key?(name)
+          hash[name] += size
+        else
+          hash[name] = size
+        end
+      end
+    end
+    hash
+  end
+  
 end
