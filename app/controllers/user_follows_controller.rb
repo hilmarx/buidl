e650c1919a4916135b594df330d3ccce9d143ecf
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-19 20:01:23 +0800

    It all works apart from update. Backing it up when it's this good before I really fuck things up.

--- app/controllers/user_follows_controller.rb
+++ app/controllers/user_follows_controller.rb
@@ -10,5 +10,31 @@ class UserFollowsController < ApplicationController
   end
 
   def update
+    ufa = UserFollow.all
+    ufa.each do |uf|
+      # raise
+      if uf.leader_id == params[:profile_id].to_i && uf.follower_id == current_user.id
+        # raise
+        if uf.status == true
+          @id = uf.id
+          break
+        elsif uf.status == false
+          uf.status = true
+          @id = uf.id
+          break
+        end
+      end
+    end
+    @profile = Profile.find(params[:profile_id])
+    @ufo = UserFollow.find(@id)
+    if @ufo.status == true
+      @ufo.status = false
+    elsif @ufo.status == false
+      @ufo.status = true
+    end
+    @ufo.update
+    raise
+    redirect_to profile_path(@profile)
   end
+
 end
