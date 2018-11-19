32b6510a17b71ef144778eb1953564672642aace
Jethro Williams <jethrowilliams4@hotmail.com>
2018-11-19 12:03:50 +0800

    Commit in order to merge-in the master branch

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,9 +5,13 @@ class ProfilesController < ApplicationController
   def create
     if Profile.find_by(github_username: profile_params[:github_username]).present?
       @profile = Profile.find_by(github_username: profile_params[:github_username])
+      if @profile.user == nil
+        get_profile_user_id
+      end
       redirect_to profile_path(@profile)
     else
       @profile = Profile.new(profile_params)
+      get_profile_user_id
       @profile.save!
       redirect_to profile_path(@profile)
     end
@@ -16,12 +20,22 @@ class ProfilesController < ApplicationController
 
   def show
     @profile = Profile.find(params[:id])
-    if current_user != nil && current_user.github_username == @profile.github_username
-      @profile.user = current_user
+    if @profile.user == nil
+      get_profile_user_id
     end
   end
 
   def profile_params
     params.require(:profile).permit(:github_username)
   end
+
+  def get_profile_user_id
+    users = User.all
+    users.each do |u|
+      if u.github_username == @profile.github_username
+        @profile.user = u
+      end
+    end
+  end
+
 end
