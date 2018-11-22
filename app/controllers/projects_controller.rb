fefaf638b2b933cabe7be1cad18354c3bcfbb2ce
Hilmar Orth <hm.orth@aol.com>
2018-11-22 22:08:46 +0800

    restarted APi then bug was fixed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,17 +20,12 @@ class ProjectsController < ApplicationController
   # end
 
   def show
-    url = "https://api.github.com/repos/hilmarx/offthewagon"
-    project_details_serialized = open(url).read
-    project = Project.find(params[:id])  # make pundit happy
-    authorize project
-    @project_details = JSON.parse(project_details_serialized)
+
   end
 
   def update
     @profile = Profile.find(params[:profile_id])
     @project = Project.find(params[:id])
-
     authorize @project
     @project.update(project_params)
     redirect_to profile_path(@profile)
