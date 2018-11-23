b47a5bee4db55f6d70369676dc43d99d569bac4b
william oleson <wnoleson@gmail.com>
2018-11-23 14:24:54 +0800

    search feature implemented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,21 +20,30 @@ class ProfilesController < ApplicationController
   end
 
   def show
-    @user = current_user
-    @project = Project.new
-    @profile = Profile.find(params[:id])
-    authorize @profile
-    authorize @project
-    @user_follows = UserFollow.all
-    # @user_follow_test = UserFollow.where()
-    if @profile.user == nil
-      get_profile_user_id
+    if params[:search]
+      github_username = params[:search][:github_username]
+      @profile = Profile.find_by('github_username @@ ?', "%#{github_username}%")
+      unless @profile
+        redirect_back(fallback_location: root_path)
+        flash[:notice] = "Couldnt find anyone by that GitHub username"
+      end
+    else
+      @user = current_user
+      @project = Project.new
+      @profile = Profile.find(params[:id])
+      authorize @project
+      @user_follows = UserFollow.all
+      # @user_follow_test = UserFollow.where()
+      if @profile.user == nil
+        get_profile_user_id
+      end
+      # If the user has 0 projects or something went wrong w/ the API, redirect to root
+      unless @profile.projects.any?
+        redirect_to root_path
+        flash[:alert] = "Something went wrong, please try again"
+      end
     end
-    # If the user has 0 projects or something went wrong w/ the API, redirect to root
-    unless @profile.projects.any?
-      redirect_to root_path
-      flash[:alert] = "Something went wrong, please try again"
-    end
+    authorize @profile ? @profile : Profile
   end
 
   def update
