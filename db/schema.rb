07d4be2f8e7dc94630ef2fab5354d1ef138bc614
Owain Skyrme <owaingskyrme@gmail.com>
2018-11-23 17:01:43 +0800

    finished as much as posiible

--- db/schema.rb
+++ db/schema.rb
@@ -104,6 +104,15 @@ ActiveRecord::Schema.define(version: 2018_11_22_034329) do
     t.boolean "status", default: true, null: false
   end
 
+  create_table "user_technologies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "technology_id"
+    t.bigint "profile_id"
+    t.index ["profile_id"], name: "index_user_technologies_on_profile_id"
+    t.index ["technology_id"], name: "index_user_technologies_on_technology_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
